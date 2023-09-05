8a02a0dbcc6fdbe3451aafc845e049ae76305be1
jaecs <jacs.chen@gmail.com>
2023-09-05 21:42:33 +0800

    feat: check if homebrew is installed

--- setup.sh
+++ setup.sh
@@ -2,13 +2,20 @@
 
 theme="Woom"
 
-echo "checking system requirement ..."
+echo "checking system requirement..."
 if [[ $OSTYPE != "darwin"* ]]; then
   echo "Failed to run the script due to system incompatibility."
   exit 1 
 fi
 
-echo "installing homebrew..."
+echo "checking homebrew..."
+if [[ $(command -v brew) == "" ]]; then
+  echo "Installing homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  echo "Updating homebrew..."
+  brew update
+fi
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo "installing git, vim and zsh ..."
@@ -20,18 +27,18 @@ echo "installing nerd-fonts ..."
 brew tap homebrew/cask-fonts
 brew install font-hack-nerd-font
 
-echo "installing spaceship ..."
+echo "installing spaceship..."
 brew install spaceship
 echo "source $(brew --prefix)/opt/spaceship/spaceship.zsh" >>! ~/.zshrc
 
 # install libplist to use plutil to control terminal theme
-echo "installing libplist ..."
+echo "installing libplist..."
 brew install libplist
 
-echo "installing $theme theme ..."
+echo "installing $theme theme..."
 plutil -insert "Window Settings" -xml "$(curl -s https://raw.githubusercontent.com/jaecs/woom/main/themes/$theme.terminal)" ~/Library/Preferences/com.apple.Terminal.plist
 
-ehco "setting $theme default ..."
+ehco "setting $theme default..."
 defaults write com.apple.Terminal "Default Window Settings" -string "Material-Theme"
 defaults write com.apple.Terminal "Default Window Settings" -string "Material-Theme"
 

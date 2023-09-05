e38aba21c960d69e83d3a0b703b99a74c3ab7b1f
jaecs <jacs.chen@gmail.com>
2023-09-05 21:33:56 +0800

    feat: add setup.sh with woom theme

---
+++ setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+theme="Woom"
+
+echo "checking system requirement ..."
+if [[ $OSTYPE != "darwin"* ]]; then
+  echo "Failed to run the script due to system incompatibility."
+  exit 1 
+fi
+
+echo "installing homebrew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "installing git, vim and zsh ..."
+brew install git
+brew install vim
+brew install zsh
+
+echo "installing nerd-fonts ..."
+brew tap homebrew/cask-fonts
+brew install font-hack-nerd-font
+
+echo "installing spaceship ..."
+brew install spaceship
+echo "source $(brew --prefix)/opt/spaceship/spaceship.zsh" >>! ~/.zshrc
+
+# install libplist to use plutil to control terminal theme
+echo "installing libplist ..."
+brew install libplist
+
+echo "installing $theme theme ..."
+plutil -insert "Window Settings" -xml "$(curl -s https://raw.githubusercontent.com/jaecs/woom/main/themes/$theme.terminal)" ~/Library/Preferences/com.apple.Terminal.plist
+
+ehco "setting $theme default ..."
+defaults write com.apple.Terminal "Default Window Settings" -string "Material-Theme"
+defaults write com.apple.Terminal "Default Window Settings" -string "Material-Theme"
+
+ehco "Your all set. Congratulation!"
+ehco "Please close and reopen your terminal. Enjoy!"
